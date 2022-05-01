2039d3c69bdb4fd174ca8b9576976f688bcc77e9
Ahmad <huntertom417@gmail.com>
2022-05-02 03:54:45 +0500

    Now compiles entire files, sort of

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from riscv import instructions
 from _parser import get_tokens, i_type, r_type, b_type
+import sys
 
-instr = input(">")
-
-op, operands = get_tokens(instr)
-instr_type = instructions[op]['type']
-
-if instr_type=="R":
-    print(r_type(op,operands))
-elif instr_type=="I":
-    print(i_type(op,operands))
-elif instr_type=="B":
-    print(b_type(op,operands))
+if len(sys.argv)<3: #one arg is main.py
+    print("Usage: python main.py <input file> <output file>")
+else:
+    filename = sys.argv[1]
+    outname = sys.argv[2]
+    with open(filename,'r') as file:
+        out = open(outname,'w')
+        for line in file.readlines():
+            op, operands = get_tokens(line)
+            instr_type = instructions[op]['type']
+            if instr_type=="R":
+                instr = r_type(op,operands)
+            elif instr_type=="I":
+                instr =  i_type(op,operands)
+            elif instr_type=="B":
+                instr = b_type(op,operands)
+        out.write(instr+"\n")
