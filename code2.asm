a7be664f6d79d7f1ec59e76f8ddd500f4968cfae
Ahmad <huntertom417@gmail.com>
2022-05-11 23:18:33 +0500

    updated code

--- code2.asm
+++ code2.asm
@@ -1,3 +1,10 @@
-lw x1, 1(x0)
-addi x2, x1, 1
-beq x0,x0, 0
+add x1, x0, x0
+add x2, x0, x0
+addi x3, x0, 11
+slt x4, x3, x1
+bne x4,x0, 16
+add x2, x2, x1
+addi x1,x1, 1
+beq x0, x0, -16
+add x31,x2,x0
+beq x31, x31, 0
